24650280a49d8d86d029e6531c7c6286c515d702
Adam Ringeisen <ringeisen@gmail.com>
2022-12-01 15:13:06 -0500

    Day 1 part 1 done

--- main.go
+++ main.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+var per_elf = 0
+var richest_elf = 0
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+
+
 
 func main() {
-	fmt.Println("Hello, world!")
+	
+	dat, err := os.Open("day_1_input.txt")
+	check(err)
+	defer dat.Close()
+
+	scanner := bufio.NewScanner(dat)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		if line == "" {
+			if per_elf > richest_elf {
+				richest_elf = per_elf
+			}
+			per_elf = 0
+		} else {
+			calories, err := strconv.Atoi(line)
+			check(err)
+			per_elf += calories
+		}
+	}
+	fmt.Printf("The Elf with the most calories has %d of them\n", richest_elf)
 }
