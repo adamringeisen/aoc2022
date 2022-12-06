5b1b4f5289a80be06b26bca766f77e3e06a665b8
Adam Ringeisen <ringeisen@gmail.com>
2022-12-06 10:36:50 -0500

    Day 4 part one done

--- day-4/part-1/main.go
+++ day-4/part-1/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
-	"bufio"
+	"strconv"
+	"strings"
 )
 
 func get_input() string {
@@ -14,18 +16,32 @@ func get_input() string {
 	}
 }
 
-func get_lines() []string {
+func get_lines() [][]string {
 	input := get_input()
 	data, err := os.Open(input)
 	check(err)
-	var lines []string
+	var lines [][]string
 	scanner := bufio.NewScanner(data)
 	for scanner.Scan() {
-		lines = append(lines, scanner.Text())
+		splitty := strings.Split(scanner.Text(), ",")
+		lines = append(lines, splitty)
+
 	}
 	return lines
 }
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
 
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
 
 func check(e error) {
 	if e != nil {
@@ -33,6 +49,46 @@ func check(e error) {
 	}
 }
 
+func just_answer(amin, amax, bmin, bmax int) bool {
+	// find larger range
+	arange := amax - amin
+	brange := bmax - bmin
+	// if a is bigger
+	if arange > brange {
+		if bmin >= amin && bmax <= amax {
+			//			fmt.Printf("A > B [%d, %d], [%d, %d] True\n", amin, amax, bmin, bmax) 
+			return true
+		}
+	} else
+	// if b is bigger
+	{
+		if amin >= bmin && amax <= bmax {
+			// fmt.Printf("B > A [%d, %d], [%d, %d] True\n", amin, amax, bmin, bmax)
+			return true
+		}
+	}
+	// fmt.Printf("AR: %d, BR: %d - [%d, %d], [%d, %d] False\n", arange, brange,amin, amax, bmin, bmax)
+	return false
+}
+
+func cv(s string) int {
+	a, e := strconv.Atoi(s)
+	check(e)
+	return a
+}
+
 func main() {
-	fmt.Println("Hello World")
+	lines := get_lines()
+	result := 0
+
+	for _, line := range lines {
+		a := strings.Split(line[0], "-")
+		b := strings.Split(line[1], "-")
+
+		if just_answer(cv(a[0]), cv(a[1]), cv(b[0]), cv(b[1])) {
+			result++
+		}
+	}
+	fmt.Printf("Result is: %d\n", result)
+
 }
